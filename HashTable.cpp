c699f8b6684e3da4be3ac8b2de5662c9aa417457
ishandubey22 <135635193+ishandubey22@users.noreply.github.com>
2024-10-19 21:24:16 +0530

    Update HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,207 +1,166 @@
 #include <iostream>
-#include <cmath>
+#include <algorithm>
 using namespace std;
 
-class CustomHashTable
+class HashTable
 {
-    int capacity;
-    int *hashArray;
-    int numElements;
-    float loadFactor; // Load factor for resizing
+    int size;
+    int *arr1;
+    int input;
+    float alpha;       //alpha= load
 
 public:
-    // Constructor for the hash table
-    CustomHashTable(int initialSize)
+    HashTable(int x)
     {
-        capacity = initialSize;
-        hashArray = new int[capacity];
-        loadFactor = 0.8;
-        numElements = 0;
+        size = x;
+        arr1 = new int[size];
+        alpha = 0.8;
+        input = 0;
+        
 
-        // Set all slots as empty initially
-        for (int i = 0; i < capacity; i++)
+        // initially set every input to -1
+        for (int i = 0; i < size; i++)
         {
-            hashArray[i] = -1;
+            arr1[i] = -1;
         }
     }
 
-    // Resize function to find the next prime number at least twice the current size
-    int getNextPrime(int currentSize)
-    {
-        int nextSize = currentSize * 2;
-        while (!isPrime(nextSize))
-        {
-            nextSize += 1;
-        }
-        return nextSize;
-    }
 
-    // Helper function to check if a number is prime
-    bool isPrime(int num)
+    int resize(int size_current)    //Next prime number atleats twice the current size
     {
-        if (num <= 1)
-            return false;
-        for (int i = 2; i * i <= num; i++)
+        int no = size_current * 2;
+        while (isPrime(no) != true)
         {
-            if (num % i == 0)
+            no += 1;
+        }
+        return no;
+    }
+    
+    bool isPrime(int no)        //To check if number is prime
+    {
+        int c = 0;      //counter
+        for (int i = 1; i <= no; i++)
+        {
+            if (no % i == 0)
             {
-                return false;
+                c += 1;
             }
         }
-        return true;
+        return c == 2;
     }
 
-    // Function to resize the hash table when load factor threshold is exceeded
-    void resizeTable()
+    void Resizing()
     {
-        int newCapacity = getNextPrime(capacity); // New capacity after resizing
-        int *newArray = new int[newCapacity];
+        int newsz = resize(size);        //size of resized table
+        int *arr2 = new int[newsz];
 
-        // Initialize the new array
-        for (int i = 0; i < newCapacity; i++)
+        // initially set values to -1
+        for (int i = 0; i < newsz; i++)
         {
-            newArray[i] = -1;
+            arr2[i] = -1;
         }
 
-        // Rehash the elements into the new table
-        for (int i = 0; i < capacity; i++)
+        for (int i = 0; i < size; i++)
         {
-            if (hashArray[i] != -1)
+            if (arr1[i] != -1)
             {
-                int value = hashArray[i];
-                int newIndex = value % newCapacity;
+                int key = arr1[i];
+                int index = key % newsz;
 
-                // Apply quadratic probing to find the next available slot
-                int offset = 0;
-                while (newArray[(newIndex + offset * offset) % newCapacity] != -1 && offset <= ((newCapacity + 1) / 2))
+                // using quadratic probing
+                int j = 0;
+                while (arr2[(index + j * j) % newsz] != -1 && j <= ((newsz + 1) / 2))
                 {
-                    offset += 1;
+                    j += 1;
                 }
-                int finalPos = (newIndex + offset * offset) % newCapacity;
-                if (newArray[finalPos] == -1)
-                    newArray[finalPos] = value;
+                int pos = (index + j * j) % newsz;
+                if (arr2[pos] == -1)
+                    arr2[pos] = key;
                 else
                     cout << "Max probing limit reached!" << endl;
             }
         }
 
-        delete[] hashArray; // Free the old memory
-        hashArray = newArray;
-        capacity = newCapacity;
+        delete[] arr1; // delete the older array
+        arr1 = arr2;
+        size = newsz;
     }
 
-    // Insert function to add elements to the hash table
-    void insertKey(int value)
+    void insert(int key)
     {
-        float currentLoadFactor = numElements / (float)capacity; // Compute current load factor
-        if (currentLoadFactor >= loadFactor)
+        float new_alpha = input / (float)size; // typecast to float type
+        if (new_alpha >= alpha)
         {
-            resizeTable(); // Trigger resizing if load factor exceeds the threshold
+            Resizing();         //resize
         }
 
-        int startIndex = value % capacity;
-        int offset = 0;
-
-        // Use quadratic probing to resolve collisions
-        while (hashArray[(startIndex + offset * offset) % capacity] != -1 && offset <= ((capacity + 1) / 2))
+        int index = key % size;
+        int i = 0;
+        while (arr1[(index + i * i) % size] != -1 && i <= ((size + 1) / 2))
         {
-            if (hashArray[(startIndex + offset * offset) % capacity] == value)
+            if (arr1[(index + i * i) % size] == key)
             {
                 cout << "Duplicate key insertion is not allowed" << endl;
                 return;
             }
-            offset += 1;
+            i += 1;
         }
-
-        int finalIndex = (startIndex + offset * offset) % capacity;
-        if (hashArray[finalIndex] == -1)
-        {
-            hashArray[finalIndex] = value;
-            numElements += 1;
-        }
+        int pos = (index + i * i) % size;
+        if (arr1[pos] == -1)
+            {arr1[pos] = key;
+            input += 1;}
         else
         {
             cout << "Max probing limit reached!" << endl;
+            return;
         }
     }
 
-    // Search for an element in the hash table
-    int searchKey(int value)
-    {
-        int startIndex = value % capacity;
-        int offset = 0;
 
-        // Apply quadratic probing while searching for the key
-        while (hashArray[(startIndex + offset * offset) % capacity] != value && offset <= ((capacity + 1) / 2))
+    int search(int key)             //function to search
+    {
+        int index = key % size;
+        int i = 0;
+
+        while (arr1[(index + i * i) % size] != key && i <= ((size + 1) / 2))
         {
-            if (hashArray[(startIndex + offset * offset) % capacity] == -1)
+            if (arr1[(index + i * i) % size] == -1)
             {
-                return -1; // Element not found
+                return -1;
             }
-            offset++;
+            i++;
         }
-
-        if (hashArray[(startIndex + offset * offset) % capacity] == value)
-            return (startIndex + offset * offset) % capacity;
+        if (arr1[(index + i * i) % size] == key)
+            return (index + i * i) % size;
         else
-            return -1; // Element not found
+            return -1;
     }
-
-    // Function to remove an element from the hash table
-    void deleteKey(int value)
+    
+    void remove(int key)
     {
-        int position = searchKey(value); // Search for the element
-        if (position != -1)
+        int x = search(key);        //Element to remove
+        if (x != -1)
         {
-            hashArray[position] = -1; // Mark the position as empty
-            numElements--;
+            arr1[x] = -1;
+            input--;
         }
         else
-        {
             cout << "Element not found" << endl;
-        }
     }
 
-    // Print the contents of the hash table
-    void printHashTable()
+    void printTable()
     {
-        for (int i = 0; i < capacity; i++)
+        for (int i = 0; i < size; i++) 
         {
-            if (hashArray[i] == -1)
+            if (arr1[i] == -1)
             {
-                cout << "- "; // Empty slot
+                cout << "- ";
             }
             else
             {
-                cout << hashArray[i] << " "; // Occupied slot
+                cout << arr1[i] << " ";
             }
         }
         cout << endl;
     }
-
-    // Destructor to release dynamically allocated memory
-    ~CustomHashTable()
-    {
-        delete[] hashArray;
-    }
-}; // End of class definition
-
-int main()
-{
-    CustomHashTable ht(7); // Create hash table with initial size 7
-
-    ht.insertKey(10);
-    ht.insertKey(20);
-    ht.insertKey(30);
-    ht.insertKey(25);
-    ht.insertKey(35);
-
-    cout << "Hash Table contents after insertion: ";
-    ht.printHashTable();
-
-    ht.deleteKey(25);
-    cout << "Hash Table contents after removing 25: ";
-    ht.printHashTable();
-
-    return 0;
-}
+};
